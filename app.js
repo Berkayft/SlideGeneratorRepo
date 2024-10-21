31e0ed48651d43d02be69ff4398fc50ca0c3a8d4
Berkaytekin <berkayfehmit@gmail.com>
2024-10-22 00:10:19 +0300

    just api

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+require("dotenv").config();
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+app.get("/", (req, res) => {
+    res.send("Hello, welcome to the text generation API!");
+});
+
+app.get('/generate-text', async (req, res) => {
+    const prompt = req.query.prompt || "Hello, my name is Berkay";
+
+    try {
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        
+        res.json({ generatedText: text });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred', error: error.message });
+    }
+});
+
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log("Server running on port " + PORT);
+    else
+        console.log("Error occurred, server can't start", error);
+});
